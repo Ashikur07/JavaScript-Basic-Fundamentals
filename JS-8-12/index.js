09c92dcb9065d736b79d230d4f1c347b3a58f3b4
Ashikur07 <ashik.ict.iu@gmail.com>
2025-12-08 22:52:05 +0600

    concept of global execution context

--- JS-8-12/index.js
+++ JS-8-12/index.js
@@ -1,9 +1,11 @@
-// var , let , const
-
-console.log(a);
-
-var a = 10;
-// console.log(a);
+// Execution context 
+var n=2;
+function square(num){
+    var ans = num*num;
+    return ans;
+}
+var square2 = square(n);
+var square4 = square(4);
 
 
 
