78500925a5055a9751cd6916c03ef25e50e6bb06
Ashikur07 <ashik.ict.iu@gmail.com>
2025-12-10 11:01:39 +0600

    Not redeclare use let

--- JS-10-12/let&const.js
+++ JS-10-12/let&const.js
@@ -1,4 +1,7 @@
-console.log(a); // if we declare a variable use let we cannto access before a is decalre 
-// In this case line 1-3 we cannot acces a that is called temporary red Zone
+console.log(a); // if we declare a variable use let we cannto access before a is decalre show an reference error
+// In this case line 1-4 we cannot acces a that is called temporary red Zone
+// but we can acces a after line 5  
 
-let a = 10;
+let a = 10; 
+
+let a = 1000;
