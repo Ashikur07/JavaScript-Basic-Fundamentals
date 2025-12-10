f36f116d318adcb12643288c615b671581f627a3
Ashikur07 <ashik.ict.iu@gmail.com>
2025-12-10 10:59:44 +0600

    Temporary Red Zone

---
+++ JS-10-12/let&const.js
@@ -0,0 +1,4 @@
+console.log(a); // if we declare a variable use let we cannto access before a is decalre 
+// In this case line 1-3 we cannot acces a that is called temporary red Zone
+
+let a = 10;
