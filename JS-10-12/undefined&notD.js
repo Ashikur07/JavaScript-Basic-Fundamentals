1c8cc3dbe3fac4a3012c2362f1e37492ad27acab
Ashikur07 <ashik.ict.iu@gmail.com>
2025-12-10 09:10:34 +0600

    show not defined

--- JS-10-12/undefined&notD.js
+++ JS-10-12/undefined&notD.js
@@ -2,3 +2,4 @@ console.log(x);
 
 var x = 10; // even before the line is executed the memory is alocated to x variable and place an special keyword "undefined"
 
+console.log(a); // if we try to acces a that is not defined then show and error "a is not defined"
