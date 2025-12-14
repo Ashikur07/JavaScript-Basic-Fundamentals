55d6a883f56a622a2f38a7ddb8d1d696491db082
Ashikur07 <ashik.ict.iu@gmail.com>
2025-12-14 10:07:36 +0600

    concept of shadowing

--- JS-14-12/block.js
+++ JS-14-12/block.js
@@ -1,12 +1,16 @@
+let b=100;
+
 {
     // this is block that combined multiple state in one group
     var a =10;
-    let b=20;
+    let b=20;  // shadowning
     const c=30;
     console.log(a);
     console.log(b);
     console.log(c);
 }
 
-console.log(a);  // access variable a
-console.log(b);   // cannot access because block scope
+// console.log(a);  // access variable a
+// console.log(b);   // cannot access because block scope
+
+console.log(b);
