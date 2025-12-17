0d454451a009b1a3b45b153adcbfe155b536685b
Ashikur07 <ashik.ict.iu@gmail.com>
2025-12-17 10:11:24 +0600

    another example of shadowing

--- JS-14-12/block.js
+++ JS-14-12/block.js
@@ -1,13 +1,16 @@
 let b=100;
+var x= 20;
 
 {
     // this is block that combined multiple state in one group
+    let x =30;
     var a =10;
     let b=20;  // shadowning
     const c=30;
     console.log(a);
     console.log(b);
     console.log(c);
+    console.log(x);
 }
 
 // console.log(a);  // access variable a
