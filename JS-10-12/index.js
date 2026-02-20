d1c5ca6480e84551c63261f775da0d5a2cf270b6
Ashikur07 <ashik.ict.iu@gmail.com>
2026-02-20 10:39:09 +0600

    repeat hoisting in js

--- JS-10-12/index.js
+++ JS-10-12/index.js
@@ -1,14 +1,3 @@
 // Hoisting in JavaScript 🔥(variables & functions)
 
-getName();
-console.log(x);
-
-var x = 7;
-// function getName(){
-//     console.log("JS Fundamentals")
-// }
-
-var getName = () =>{
-    console.log("JS Fundamentals")
-}
 
