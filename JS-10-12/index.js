f79b80925faa8937a12aad2743e0e95710960d0c
Ashikur07 <ashik.ict.iu@gmail.com>
2026-02-20 10:45:15 +0600

    concept of hoisting

--- JS-10-12/index.js
+++ JS-10-12/index.js
@@ -1,10 +1,12 @@
 // Hoisting in JavaScript 🔥(variables & functions)
 
+getName();
+console.log(x);
+
 var x = 7;
 
 function getName(){
     console.log("Hello JS")
 }
 
-getName();
-console.log(x);
+
